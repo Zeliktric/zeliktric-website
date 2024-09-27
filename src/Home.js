097ad3a2ca960b9cd35b33934ab62c73a98148f0
Zeliktric <zeliktric@gmail.com>
2024-09-27 13:20:54 +0100

    website changes

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,14 @@
-import { useRef, useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
+import { Tooltip } from "react-tooltip";
+
+import github from "./Images/github.png";
+import youtube from "./Images/youtube.png";
+import instagram from "./Images/instagram.png";
+import x from "./Images/x.png";
+import twitter from "./Images/twitter.png";
+import discord from "./Images/discord.png";
 
 export default function Home() {
-    const list = ["Full-stack Developer", "Game Dev Hobbyist", "Gamer", "Man United Fan", "Creative", "Sporty"];
-
     const background = useRef(null);
     const bolts = useRef(null);
     const name = useRef(null);
@@ -11,27 +17,12 @@ export default function Home() {
     // Sleep/delay function
     const timer = ms => new Promise(res => setTimeout(res, ms));
 
+    const [bird, setBird] = useState(x);
+
     useEffect(() => {
         async function startAnimation() {
-            // if(bolts.current.style != null) {
-            //     name.current.style.cssText = "opacity: 0%";
-            //     bolts.current.style.cssText = "top: -50em !important;";
-                
-            // }
 
-            // await timer(1000);
-
-            // if(bolts.current != null) {
-            //     bolts.current.style.cssText = "";
-            //     bolts.current.classList.add("show");
-            // }
-
-            // await timer(1500);
-            
-            // name.current.style.cssText = "transition: opacity 1s, background-size 1s";
-            // name.current.classList.add("show");
-
-            await timer(5000);
+            await timer(2500);
 
             background.current.classList.add("fade");
             bolts.current.classList.remove("show");
@@ -45,6 +36,14 @@ export default function Home() {
         startAnimation();
     }, [])
 
+    const hoverBird = (b) => {
+        if(b) {
+            setBird(twitter);
+        } else {
+            setBird(x);
+        }
+    }
+
     return <>
         <div className="main">
             <div className="background" ref={background}>
@@ -53,35 +52,55 @@ export default function Home() {
             </div>
             
             <div className="homeText hide" ref={homeText}>
-                <p className="title">Hi, I'm <span id="zeliktric"><b>Zeliktric</b></span></p>
-                <p className="subtitle">{list[Math.floor(Math.random() * list.length)]}</p>
+                <p className="title">Hi, I'm <span className="yellow"><b>Zeliktric!</b></span></p>
+                <p className="subtitle">Full-stack developer and game dev hobbyist</p>
 
                 <br/>
 
-                <div className="navigation">
-                    <div className="navLink active">
-                        <p>Home</p>
-                        <div className="underline"></div>
-                    </div>
-                    <div className="navLink">
-                        <p>About</p>
-                        <div className="underline"></div>
-                    </div>
-                    <div className="navLink">
-                        <p>Projects</p>
-                        <div className="underline"></div>
-                    </div>
-                    <div className="navLink">
-                        <p>Contact</p>
-                        <div className="underline"></div>
-                    </div>
+                <div className="about">
+                    <p>
+                        I am enthusiastic about developing programs in Python, C#, JavaScript (React) and Java. <br/><br/>
+                        <span className="yellow">Python</span>: Made Discord bots and functional websites using discord.py & quart.<br/>
+                        <span className="yellow">C#</span>: Experience using Unity to make games. <br/>
+                        <span className="yellow">JavaScript (React)</span>: Started using React as a frontend framework to make more functional websites. <br/>
+                        <span className="yellow">Java</span>: Used as part of coursework to code a robot maze and develop data structures for an application. 
+                    </p>
                 </div>
             </div>
-            
 
-            {/* <div className="zeliktricLogo">
-                <img src={zeliktricLogo} alt="zeliktric logo"/>
-            </div> */}
+            <div className="socials">
+                <div className="social">
+                    <a href="https://github.com/zeliktric" target="_blank" rel="noreferrer">
+                        <img src={github} />
+                    </a>
+                </div>
+
+                <div className="social">
+                    <a href="https://youtube.com/@zeliktric" target="_blank" rel="noreferrer">
+                        <img src={youtube} />
+                    </a>
+                </div>
+
+                <div className="social">
+                    <a href="https://instagram.com/zeliktric" target="_blank" rel="noreferrer">
+                        <img src={instagram} />
+                    </a>
+                </div>
+
+                <div className="social" onMouseOver={() => hoverBird(true)} onMouseOut={() => hoverBird(false)}>
+                    <a href="https://x.com/zeliktric" target="_blank" rel="noreferrer">
+                        <img src={bird} />
+                    </a>
+                </div>
+
+                <div className="social">
+                    <Tooltip id="discord"/>
+                    <a target="_blank" rel="noreferrer" data-tooltip-id="discord" data-tooltip-content="@zeliktric" data-tooltip-place="top">
+                        <img src={discord} />
+                    </a>
+                </div>
+                
+            </div>
         </div>
         
     </>
